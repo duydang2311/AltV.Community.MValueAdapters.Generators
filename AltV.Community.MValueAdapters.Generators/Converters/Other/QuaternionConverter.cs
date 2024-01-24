7685a589a40d54bbe9b77a95b5c7351d3ac5a9df
deluvas1911 <deluvas1911@gmail.com>
2024-01-24 10:59:20 +0100

    feat: Quaternion converter

---
+++ AltV.Community.MValueAdapters.Generators/Converters/Other/QuaternionConverter.cs
@@ -0,0 +1,103 @@
+using System.Text;
+using AltV.Community.MValueAdapters.Generators.Models;
+using AltV.Community.MValueAdapters.Generators.Utils;
+
+namespace AltV.Community.MValueAdapters.Generators.Converters;
+
+internal class QuaternionConverter : BaseConverter
+{
+    public override string[] AdditionalUsings() => ["System.Numerics"];
+
+    protected override void GenerateItemWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, "writer.BeginObject();");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("X", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)value.X);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("Y", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)value.Y);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("Z", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)value.Z);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("W", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)value.W);");
+        stringBuilder.AppendLine(indentation, "writer.EndObject();");
+    }
+
+    protected override void GenerateItemReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        var tmpNames = NameRandomizer.Get(5);
+
+        stringBuilder.AppendLine(indentation, $"float {tmpNames[0]} = 0f, {tmpNames[1]} = 0f, {tmpNames[2]} = 0f, {tmpNames[3]} = 0f;");
+        stringBuilder.AppendLine(indentation, "reader.BeginObject();");
+        stringBuilder.AppendLine(indentation, "while (reader.HasNext())");
+        stringBuilder.AppendLine(indentation++, "{");
+        stringBuilder.AppendLine(indentation, $"var {tmpNames[4]} = reader.NextName();");
+        stringBuilder.AppendLine(indentation, $"switch ({tmpNames[4]})");
+        stringBuilder.AppendLine(indentation++, "{");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("X", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[0]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("Y", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[1]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("Z", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[2]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("W", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[3]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, "default:");
+        stringBuilder.AppendLine(indentation, "reader.SkipValue();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation--, "}");
+        stringBuilder.AppendLine(indentation--, "}");
+        stringBuilder.AppendLine(indentation, "reader.EndObject();");
+        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = new Quaternion({tmpNames[0]}, {tmpNames[1]}, {tmpNames[2]}, {tmpNames[3]});");
+    }
+
+    protected override void GenerateCollectionWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, "writer.BeginObject();");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("X", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)item.X);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("Y", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)item.Y);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("Z", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)item.Z);");
+        stringBuilder.AppendLine(indentation, $"writer.Name(\"{NamingConventionHelpers.GetName("W", classInfo.NamingConvention)}\");");
+        stringBuilder.AppendLine(indentation, "writer.Value((float)item.W);");
+        stringBuilder.AppendLine(indentation, "writer.EndObject();");
+    }
+
+    protected override void GenerateCollectionReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        var tmpNames = NameRandomizer.Get(6);
+
+        stringBuilder.AppendLine(indentation, $"float {tmpNames[0]} = 0f, {tmpNames[1]} = 0f, {tmpNames[2]} = 0f, {tmpNames[3]} = 0f;");
+        stringBuilder.AppendLine(indentation, "reader.BeginObject();");
+        stringBuilder.AppendLine(indentation, "while (reader.HasNext())");
+        stringBuilder.AppendLine(indentation++, "{");
+        stringBuilder.AppendLine(indentation, $"var {tmpNames[4]} = reader.NextName();");
+        stringBuilder.AppendLine(indentation, $"switch ({tmpNames[4]})");
+        stringBuilder.AppendLine(indentation++, "{");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("X", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[0]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("Y", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[1]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("Z", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[2]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, $"case \"{NamingConventionHelpers.GetName("W", classInfo.NamingConvention)}\":");
+        stringBuilder.AppendLine(indentation, $"{tmpNames[3]} = (float)reader.NextDouble();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation++, "default:");
+        stringBuilder.AppendLine(indentation, "reader.SkipValue();");
+        stringBuilder.AppendLine(indentation--, "continue;");
+        stringBuilder.AppendLine(indentation--, "}");
+        stringBuilder.AppendLine(indentation--, "}");
+        stringBuilder.AppendLine(indentation, "reader.EndObject();");
+        stringBuilder.AppendLine(indentation, $"var {tmpNames[5]} = new Quaternion({tmpNames[0]}, {tmpNames[1]}, {tmpNames[2]}, {tmpNames[3]});");
+        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add({tmpNames[5]});");
+    }
+}
