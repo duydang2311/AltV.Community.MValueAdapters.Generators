98d2132008153a7113ecc0eaedf0550731b5068e
xLuxy <67131061+xLuxy@users.noreply.github.com>
2024-04-01 19:23:12 +0200

    feat: Add support for Guid

---
+++ AltV.Community.MValueAdapters.Generators/Converters/Text/GuidConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using AltV.Community.MValueAdapters.Generators.Models;
+
+namespace AltV.Community.MValueAdapters.Generators.Converters;
+
+internal class GuidConverter : BaseConverter
+{
+    protected override void GenerateItemWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, $"writer.Value(value.{propertyInfo.Name}.ToString());");
+    }
+
+    protected override void GenerateItemReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = Guid.Parse(reader.NextString());");
+    }
+
+    protected override void GenerateCollectionWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, "writer.Value(item.ToString());");
+    }
+
+    protected override void GenerateCollectionReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
+    {
+        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add(Guid.Parse(reader.NextString()));");
+    }
+}
