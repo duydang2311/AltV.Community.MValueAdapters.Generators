5b2457a6961ba7e4c5e3e3150df1de0ca93001b6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-04-02 10:42:40 +0700

    refactor(DateTimeConverter): keep DateTime as local time

--- AltV.Community.MValueAdapters.Generators/Converters/Other/DateTimeConverter.cs
+++ AltV.Community.MValueAdapters.Generators/Converters/Other/DateTimeConverter.cs
@@ -9,21 +9,21 @@ internal class DateTimeConverter : BaseConverter
 
     protected override void GenerateItemWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"writer.Value(new DateTimeOffset((DateTime)value.{propertyInfo.Name}).ToUnixTimeMilliseconds());");
+        stringBuilder.AppendLine(indentation, $"writer.Value((double)new DateTimeOffset(value.{propertyInfo.Name}).ToUnixTimeMilliseconds());");
     }
 
     protected override void GenerateItemReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = DateTimeOffset.FromUnixTimeMilliseconds(reader.NextLong()).DateTime;");
+        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = DateTime.UnixEpoch.AddMilliseconds((long)reader.NextDouble()).ToLocalTime();");
     }
 
     protected override void GenerateCollectionWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"writer.Value(new DateTimeOffset((DateTime)item).ToUnixTimeMilliseconds());");
+        stringBuilder.AppendLine(indentation, $"writer.Value((double)new DateTimeOffset(item).ToUnixTimeMilliseconds());");
     }
 
     protected override void GenerateCollectionReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add(DateTimeOffset.FromUnixTimeMilliseconds(reader.NextLong()).DateTime);");
+        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add(DateTime.UnixEpoch.AddMilliseconds((long)reader.NextDouble()).ToLocalTime());");
     }
 }
