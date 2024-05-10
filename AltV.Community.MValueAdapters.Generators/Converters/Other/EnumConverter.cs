fee95b5ba88d995723e138d54d4b732a138584ae
PiepMatz <rappersinfan@gmail.com>
2024-05-10 10:26:47 +0200

    Remove bool, string and char underlying enum type differnentiation

--- AltV.Community.MValueAdapters.Generators/Converters/Other/EnumConverter.cs
+++ AltV.Community.MValueAdapters.Generators/Converters/Other/EnumConverter.cs
@@ -7,76 +7,21 @@ internal class EnumConverter : BaseConverter
 {
     protected override void GenerateItemWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"writer.Value({GetWriterValuePrefix(propertyInfo)}value{GetWriterValueSuffix(propertyInfo)});");
+        stringBuilder.AppendLine(indentation, $"writer.Value((double)value.{propertyInfo.Name});");
     }
 
     protected override void GenerateItemReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = ({propertyInfo.TypeName})reader{GetReaderSuffix(propertyInfo)};");
+        stringBuilder.AppendLine(indentation, $"c.{propertyInfo.Name} = ({propertyInfo.TypeName})reader.NextDouble();");
     }
 
     protected override void GenerateCollectionWriteCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"writer.Value({GetWriterValuePrefix(propertyInfo)}item{GetWriteCollectionItemSuffix(propertyInfo)});");
+        stringBuilder.AppendLine(indentation, $"writer.Value((double)item);");
     }
 
     protected override void GenerateCollectionReadCode(StringBuilder stringBuilder, ref int indentation, MValueClassInfo classInfo, MValuePropertyInfo propertyInfo)
     {
-        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add(({propertyInfo.TypeName})reader.{GetReaderSuffix(propertyInfo)});");
+        stringBuilder.AppendLine(indentation, $"{propertyInfo.Name}Builder.Add(({propertyInfo.TypeName})reader.NextDouble();");
     }
-
-	/// <summary>
-	/// Returns a possible prefix/cast for the ItemWriteCode.
-	/// Basically only performs a "double" or "bool" cast, if
-	/// the underlying type is neither a number nor a bool.
-	/// </summary>
-	/// <param name="propertyInfo"></param>
-	/// <returns></returns>
-	private string GetWriterValuePrefix(MValuePropertyInfo propertyInfo)
-	{
-		if (propertyInfo.UnderlyingEnumTypeName == "char") return "";
-		else if (propertyInfo.UnderlyingEnumTypeName == "string") return "";
-		else if (propertyInfo.UnderlyingEnumTypeName == "bool") return "(bool)";
-		else
-			return "(double)";
-	}
-
-	/// <summary>
-	/// Returns the suffix for the value writer.
-	/// </summary>
-	/// <param name="propertyInfo"></param>
-	/// <returns></returns>
-	private string GetWriterValueSuffix(MValuePropertyInfo propertyInfo)
-	{
-		if (propertyInfo.UnderlyingEnumTypeName == "char") return $".{propertyInfo.Name}.ToString()";
-		else
-			return $".{propertyInfo.Name}";
-	}
-
-	/// <summary>
-	/// Returns the suffix for the write collection code of the item
-	/// </summary>
-	/// <param name="propertyInfo"></param>
-	/// <returns></returns>
-	private string GetWriteCollectionItemSuffix(MValuePropertyInfo propertyInfo)
-	{
-		if (propertyInfo.UnderlyingEnumTypeName == "char") return $".ToString()";
-		else
-			return "";
-	}
-
-	/// <summary>
-	/// Returns the string for the reader to target next type.
-	/// For texts (string / char), "FirstOrDefault" is appended.
-	/// </summary>
-	/// <param name="propertyInfo"></param>
-	/// <returns></returns>
-	private string GetReaderSuffix(MValuePropertyInfo propertyInfo)
-	{
-		if (propertyInfo.UnderlyingEnumTypeName == "char") return ".NextString()";
-		else if (propertyInfo.UnderlyingEnumTypeName == "string") return ".NextString().FirstOrDefault()";
-		else if (propertyInfo.UnderlyingEnumTypeName == "bool") return ".NextBool()";
-		else
-			return ".NextDouble()";
-	}
 }
